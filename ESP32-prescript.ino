1817c8de7fb988da7dd56da5f7ada5bdb71e3f5c
Too-Zestyy <violet.steers.dev@gmail.com>
2026-04-09 00:29:36 +0100

    feat: looping `_CLEAR_` animation
    Starts a looping animation on the connected OLED mimicking that within the modern index IDs in-game.

---
+++ ESP32-prescript.ino
@@ -0,0 +1,98 @@
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+const int SCREEN_WIDTH = 128;
+const int SCREEN_HEIGHT = 64;
+
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+bool buzzerState = false;
+
+// TODO: Change to char[]
+String message = "_CLEAR_";
+
+const int ledPin = 8; 
+const int buzzerPin = 20;
+
+
+/*
+  Gets a random ASCII character that is displayable using a standard font.
+*/
+char getRandomDisplayableChar() {
+  // ! - ~
+  return (char) random(33, 127);
+}
+
+/*
+  Draws a character array to the centre of the screen, with text aligned to the centre around the anchor.
+*/
+void drawCentredChars(const char *buf, int textSize)
+{
+    oled.setTextSize(textSize);
+
+    int half_screen_width = SCREEN_WIDTH / 2;
+    int half_screen_height = SCREEN_HEIGHT / 2;
+    int charcount = strlen(buf);
+
+    int16_t x1, y1;
+    uint16_t w, h;
+    oled.getTextBounds(buf, 0, 0, &x1, &y1, &w, &h); //calc width of new string
+
+    oled.setCursor(half_screen_width - w / 2, half_screen_height - h/2);
+    oled.print(buf);
+}
+
+// TODO: Maybe use char array and look for null terminator?
+void animateWordDisplay(String word, int randomCharFlipCount, int charChangeDelayMs) {
+  unsigned int bufLen = word.length();
+
+  // Allocate length of string plus constant null termination for use by OLED library to see end of string
+  char displayBuf[bufLen + 1];
+  displayBuf[bufLen] = 0;
+
+  for (unsigned int i = 0; i < bufLen; i++) {
+    displayBuf[i] = getRandomDisplayableChar();
+  }
+
+  for (unsigned int i = 0; i < bufLen; i++) {
+    for (int j = 0; j < randomCharFlipCount; j++) {
+      delay(charChangeDelayMs);
+      displayBuf[i] = getRandomDisplayableChar();
+      oled.clearDisplay();
+      // oled.setCursor(0, 0);
+      // oled.setCursor(20, 20);
+      // oled.print(displayBuf);
+      drawCentredChars(displayBuf, 2);
+      
+      oled.display();
+    }
+    delay(charChangeDelayMs);
+    displayBuf[i] = word[i];
+    oled.clearDisplay();
+    // oled.setCursor(20, 20);
+    // oled.print(displayBuf);
+    drawCentredChars(displayBuf, 2);
+    oled.display();
+  } 
+}
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+  Wire.begin(6, 8);
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  // oled.setFont();
+  oled.setTextSize(2);
+  oled.setTextColor(WHITE);
+  oled.clearDisplay();
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(buzzerPin, LOW);  
+
+}
+
+void loop() { 
+  animateWordDisplay(message, 3, 33);
+  delay(3000);
+  oled.clearDisplay();
+}
